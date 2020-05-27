6fce629f07a4d2c7eb1fd2d586b36cf568550aaa
snirfern <snirfern@walla.co.il>
2020-05-27 05:23:04 +0300

    Updates

---
+++ precache-manifest.a39ec34a2d6b79e17da3ef225b91e34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95022a11b3011f79b5e814e3810c2d27",
+    "url": "/snir-fern-22-05-2020/index.html"
+  },
+  {
+    "revision": "b7351bbe9e23611a0d3a",
+    "url": "/snir-fern-22-05-2020/static/css/main.a71d2aab.chunk.css"
+  },
+  {
+    "revision": "6c4d648a21e2d78fc3e0",
+    "url": "/snir-fern-22-05-2020/static/js/2.1b8243c7.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/snir-fern-22-05-2020/static/js/2.1b8243c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7351bbe9e23611a0d3a",
+    "url": "/snir-fern-22-05-2020/static/js/main.dacc0ca7.chunk.js"
+  },
+  {
+    "revision": "09142436ba009f51b460",
+    "url": "/snir-fern-22-05-2020/static/js/runtime-main.508fff6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6089e2408b9f1b6e2c69b4284956eeda.js"
+  "/snir-fern-22-05-2020/precache-manifest.a39ec34a2d6b79e17da3ef225b91e34f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snir-fern-22-05-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
